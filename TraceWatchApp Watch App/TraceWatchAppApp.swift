296ba7508510dba2347588521309864b1b9382d1
Stephen Swart <stephenswart8@gmail.com>
2026-03-12 14:50:37 +0200

    Watch App Integration

---
+++ TraceWatchApp Watch App/TraceWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceWatchAppApp.swift
+//  TraceWatchApp Watch App
+//
+//  Created by Stephen Swart on 2026/03/12.
+//
+
+import SwiftUI
+
+@main
+struct TraceWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
